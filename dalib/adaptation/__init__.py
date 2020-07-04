ba3de3c8497670f5bf9f189928fe3b769571c2b4
Jiang Junguang <13126830206@163.com>
2020-07-04 08:35:22 +0800

    remove afn

--- dalib/adaptation/__init__.py
+++ dalib/adaptation/__init__.py
@@ -3,7 +3,6 @@ from . import dann
 from . import mdd
 from . import dan
 from . import jan
-from . import afn
 from . import mcd
 
-__all__ = ["cdan", "dann", "mdd", "dan", "jan", "afn", "mcd"]
+__all__ = ["cdan", "dann", "mdd", "dan", "jan", "mcd"]
