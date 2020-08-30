80f4699c36791b6426e833e05000f80890127c46
Jin-Ying <290895805@qq.com>
2020-08-30 23:22:48 +0800

    modify lr in visda

--- examples-da/unsupervised/mcc.sh
+++ examples-da/unsupervised/mcc.sh
@@ -22,8 +22,8 @@ CUDA_VISIBLE_DEVICES=0 python examples-da/unsupervised/mcc.py data/office-home -
 CUDA_VISIBLE_DEVICES=0 python examples-da/unsupervised/mcc.py data/office-home -d OfficeHome -s Rw -t Pr -a resnet50  --epochs 30 --seed 0 > benchmarks/mcc/OfficeHome_Rw2Pr.txt
 
 # VisDA-2017
-CUDA_VISIBLE_DEVICES=0 python examples-da/unsupervised/mcc.py data/visda-2017 -d VisDA2017 -s Synthetic -t Real -a resnet50  --epochs 30 --seed 0 --lr > benchmarks/mcc/VisDA2017.txt
-CUDA_VISIBLE_DEVICES=0 python examples-da/unsupervised/mcc.py data/visda-2017 -d VisDA2017 -s Synthetic -t Real -a resnet101 --epochs 30 --seed 0 --lr  > benchmarks/mcc/VisDA2017_resnet101.txt
+CUDA_VISIBLE_DEVICES=0 python examples-da/unsupervised/mcc.py data/visda-2017 -d VisDA2017 -s Synthetic -t Real -a resnet50  --epochs 30 --seed 0 --lr 0.002> benchmarks/mcc/VisDA2017.txt
+CUDA_VISIBLE_DEVICES=0 python examples-da/unsupervised/mcc.py data/visda-2017 -d VisDA2017 -s Synthetic -t Real -a resnet101 --epochs 30 --seed 0 --lr 0.002> benchmarks/mcc/VisDA2017_resnet101.txt
 
 # DomainNet
 CUDA_VISIBLE_DEVICES=0 python examples-da/unsupervised/mcc.py data/domainnet -d DomainNet -s c -t i -a resnet101 --epochs 30 -i 2500 -p 500 --seed 0
