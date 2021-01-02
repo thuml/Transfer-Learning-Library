97f67c60095956b0d2206d48d729e4b51d39192a
Jiang Junguang <13126830206@163.com>
2021-01-02 16:16:40 +0800

    fix bugs for function accuracy
    this bug appears in latest version of pytorch

--- tools/utils.py
+++ tools/utils.py
@@ -48,19 +48,18 @@ class ProgressMeter(object):
 
 
 def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.no_grad():
         maxk = max(topk)
         batch_size = target.size(0)
 
         _, pred = output.topk(maxk, 1, True, True)
         pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
+        correct = pred.eq(target[None])
 
         res = []
         for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
+            correct_k = correct[:k].flatten().sum(dtype=torch.float32)
+            res.append(correct_k * (100.0 / batch_size))
         return res
 
 
