34251d492ae3dd7d6b21f570bf68e7c9391c313d
YifeiJi <jiyf990330@163.com>
2021-03-14 09:25:35 +0800

    modify delta

--- ftlib/finetune/delta.py
+++ ftlib/finetune/delta.py
@@ -1,31 +1,21 @@
+import json
+import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 import functools
 from collections import OrderedDict
 
+from tqdm import tqdm
 
-def flatten_outputs(fea):
-    return torch.reshape(fea, (fea.shape[0], fea.shape[1], fea.shape[2] * fea.shape[3]))
-
-
-def reg_att_fea_map(inputs, layer_outputs_source, layer_outputs_target, model_source, channel_weights):
-    fea_loss = 0.0
-    for i, (fm_src, fm_tgt) in enumerate(zip(layer_outputs_source, layer_outputs_target)):
-        fm_src = layer_outputs_source[fm_src]
-        fm_tgt = layer_outputs_source[fm_tgt]
-        b, c, h, w = fm_src.shape
-        fm_src = flatten_outputs(fm_src)
-        fm_tgt = flatten_outputs(fm_tgt)
-        div_norm = h * w
-        distance = torch.norm(fm_tgt - fm_src.detach(), 2, 2)
-        distance = c * torch.mul(channel_weights[i], distance ** 2) / (h * w)
-        fea_loss += 0.5 * torch.sum(distance)
-    return fea_loss
+from common.utils import AverageMeter, ProgressMeter
+from common.utils.metric import accuracy
 
 
-class ClassifierRegLoss(nn.Module):
+class ClassifierRegularization(nn.Module):
     def __init__(self, model: nn.Module):
-        super(ClassifierRegLoss, self).__init__()
+        super(ClassifierRegularization, self).__init__()
         self.model = model
 
     def forward(self):
@@ -37,9 +27,9 @@ class ClassifierRegLoss(nn.Module):
         return output
 
 
-class L2spRegLoss(nn.Module):
+class L2spRegularization(nn.Module):
     def __init__(self, source_model: nn.Module, target_model: nn.Module):
-        super(L2spRegLoss, self).__init__()
+        super(L2spRegularization, self).__init__()
         self.source_model = source_model
         self.target_model = target_model
         self.source_weight = {}
@@ -53,9 +43,9 @@ class L2spRegLoss(nn.Module):
         return output
 
 
-class FeatureRegLoss(nn.Module):
+class FeatureRegularization(nn.Module):
     def __init__(self):
-        super(FeatureRegLoss, self).__init__()
+        super(FeatureRegularization, self).__init__()
 
     def forward(self, layer_outputs_source, layer_outputs_target):
         output = 0.0
@@ -66,7 +56,29 @@ class FeatureRegLoss(nn.Module):
         return output
 
 
-def rgetattr(obj, attr, *args):
+class AttentionFeatureRegularization(nn.Module):
+    def __init__(self, channel_weight):
+        super(AttentionFeatureRegularization, self).__init__()
+        self.channel_weight = channel_weight
+
+    def forward(self, layer_outputs_source, layer_outputs_target):
+        output = 0.0
+        for i, (fm_src, fm_tgt) in enumerate(zip(layer_outputs_source, layer_outputs_target)):
+            fm_src = layer_outputs_source[fm_src]
+            fm_tgt = layer_outputs_target[fm_tgt]
+
+            b, c, h, w = fm_src.shape
+            fm_src = fm_src.reshape(b, c, h * w)
+            fm_tgt = fm_tgt.reshape(b, c, h * w)
+
+            distance = torch.norm(fm_tgt - fm_src.detach(), 2, 2)
+            distance = c * torch.mul(self.channel_weight[i], distance ** 2) / (h * w)
+            output += 0.5 * torch.sum(distance)
+
+        return output
+
+
+def get_attribute(obj, attr, *args):
     def _getattr(obj, attr):
         return getattr(obj, attr, *args)
     return functools.reduce(_getattr, [obj] + attr.split('.'))
@@ -82,10 +94,9 @@ class IntermediateLayerGetter:
         ret = OrderedDict()
         handles = []
         for name in self.return_layers:
-            layer = rgetattr(self._model, name)
+            layer = get_attribute(self._model, name)
             def hook(module, input, output, name=name):
                 ret[name] = output
-
             try:
                 h = layer.register_forward_hook(hook)
             except AttributeError as e:
@@ -102,3 +113,109 @@ class IntermediateLayerGetter:
             h.remove()
 
         return ret, output
+
+
+class ChannelWeightCalculator:
+    def __init__(self, model, return_layers, criterion, data_loader, device, iteration_limit=-1):
+        self._model = model
+        self.return_layers = return_layers
+        self.channel_weight = []
+        self.criterion = criterion
+        self.data_loader = data_loader
+        self.iteration_limit = iteration_limit
+        self.device = device
+        for layer_id, name in enumerate(self.return_layers):
+            layer = get_attribute(self._model, name)
+            layer_channel_weight = [0] * layer.out_channels
+            self.channel_weight.append(layer_channel_weight)
+
+    def train_classifier(self, optimizer, scheduler, num_epochs):
+        iterations_per_epoch = len(self.data_loader)
+        self._model.train()
+        self._model.backbone.requires_grad = False
+
+        for epoch in range(num_epochs):
+            losses = AverageMeter('Loss', ':3.2f')
+            cls_accs = AverageMeter('Cls Acc', ':3.1f')
+            progress = ProgressMeter(
+                iterations_per_epoch,
+                [losses, cls_accs],
+                prefix="Epoch: [{}]".format(epoch))
+
+            for i, data in enumerate(self.data_loader):
+                inputs, labels = data
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
+                outputs, _ = self._model(inputs)
+                loss = self.criterion(outputs, labels)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                cls_acc = accuracy(outputs, labels)[0]
+
+                losses.update(loss.item(), inputs.size(0))
+                cls_accs.update(cls_acc.item(), inputs.size(0))
+
+                if i % 10 == 0:
+                    progress.display(i)
+            scheduler.step()
+
+    def calculate(self):
+        print('Calculating channel weights...')
+        self._model.eval()
+
+        if self.iteration_limit > 0:
+            total_iteration = min(len(self.data_loader), self.iteration_limit)
+        else:
+            total_iteration = len(self.data_loader)
+
+        progress = ProgressMeter(
+            total_iteration,
+            [],
+            prefix="Iteration: ")
+
+        for i, data in enumerate(self.data_loader):
+            if i >= total_iteration:
+                break
+            inputs, labels = data
+            inputs = inputs.to(self.device)
+            labels = labels.to(self.device)
+            outputs, _ = self._model(inputs)
+            loss_0 = self.criterion(outputs, labels)
+            progress.display(i)
+            for layer_id, name in enumerate(tqdm(self.return_layers)):
+                layer = get_attribute(self._model, name)
+                for j in range(layer.out_channels):
+                    tmp = self._model.state_dict()[name + '.weight'][j, ].clone()
+                    self._model.state_dict()[name + '.weight'][j, ] = 0.0
+                    outputs, _ = self._model(inputs)
+                    loss_1 = self.criterion(outputs, labels)
+                    difference = loss_1 - loss_0
+                    difference = difference.detach().cpu().numpy().item()
+                    history_value = self.channel_weight[layer_id][j]
+                    self.channel_weight[layer_id][j] = 1.0 * (i * history_value + difference) / (i + 1)
+                    self._model.state_dict()[name + '.weight'][j, ] = tmp
+
+    def save_channel_weight(self, file_path):
+        json.dump(self.channel_weight, open(file_path, 'w'))
+
+
+def calculate_channel_weight(classifier, criterion, optimizer, loader, scheduler, return_layers, device, channel_weight_path, args):
+    calculator = ChannelWeightCalculator(classifier, return_layers=return_layers,
+                                         criterion=criterion, data_loader=loader, device=device, iteration_limit=args.iteration_limit)
+    calculator.train_classifier(optimizer=optimizer, scheduler=scheduler,
+                                num_epochs=args.epochs_channel_weight)
+    calculator.calculate()
+    calculator.save_channel_weight(channel_weight_path)
+
+
+def get_channel_weight(file_path, device):
+    channel_weights = []
+    for weight in json.load(open(file_path)):
+        weight = np.array(weight)
+        weight = (weight - np.mean(weight)) / np.std(weight)
+        weight = torch.from_numpy(weight).float().to(device)
+        weight = F.softmax(weight / 5).detach()
+        channel_weights.append(weight)
+    return channel_weights
