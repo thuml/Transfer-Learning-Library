f634bea8cdb2a5b7b9402dd8a29ef98da92a8a40
microhhh <13654840887@163.com>
2021-04-03 14:27:48 +0800

    fix

--- common/utils/__init__.py
+++ common/utils/__init__.py
@@ -2,4 +2,4 @@ from .logger import CompleteLogger
 from .meter import *
 from .data import ForeverDataIterator
 
-__all__ = ['metric', 'analysis', 'meter', 'data', 'keypoint_detection', 'logger']
+__all__ = ['metric', 'analysis', 'meter', 'data', 'logger']
