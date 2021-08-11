059cf2331f95112584a47a851e8bdcf34c9b9fbf
jjg <13126830206@163.com>
2021-08-12 00:57:01 +0800

    adjust training code for openset DA

--- examples/domain_adaptation/openset/dann.sh
+++ examples/domain_adaptation/openset/dann.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Office31
-CUDA_VISIBLE_DEVICES=5 python dann.py data/office31 -d Office31 -s A -t W -a resnet50 --epochs 20 --seed 0 --threshold 0.9 --log logs/dann/Office31_A2W
+CUDA_VISIBLE_DEVICES=0 python dann.py data/office31 -d Office31 -s A -t W -a resnet50 --epochs 20 --seed 0 --threshold 0.9 --log logs/dann/Office31_A2W
 CUDA_VISIBLE_DEVICES=0 python dann.py data/office31 -d Office31 -s D -t W -a resnet50 --epochs 20 --seed 0 --threshold 0.9 --log logs/dann/Office31_D2W
 CUDA_VISIBLE_DEVICES=0 python dann.py data/office31 -d Office31 -s W -t D -a resnet50 --epochs 20 --seed 0 --threshold 0.9 --log logs/dann/Office31_W2D
 CUDA_VISIBLE_DEVICES=0 python dann.py data/office31 -d Office31 -s A -t D -a resnet50 --epochs 20 --seed 0 --threshold 0.9 --log logs/dann/Office31_A2D
@@ -23,4 +23,4 @@ CUDA_VISIBLE_DEVICES=0 python dann.py data/office-home -d OfficeHome -s Rw -t Pr
 
 # VisDA-2017
 CUDA_VISIBLE_DEVICES=0 python dann.py data/visda-2017 -d VisDA2017 -s Synthetic -t Real -a resnet50 \
-    --epochs 30 --seed 0 --center-crop --per-class-eval --log logs/dann/VisDA2017_S2R
+    --epochs 30 --seed 0 --train-resizing cen.crop --per-class-eval --log logs/dann/VisDA2017_S2R
