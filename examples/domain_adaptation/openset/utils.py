04d486ad443bb1902330338dea82cdd3ed2a9403
Jiang Junguang <13126830206@163.com>
2021-08-12 00:59:06 +0800

    Update utils.py

--- examples/domain_adaptation/openset/utils.py
+++ examples/domain_adaptation/openset/utils.py
@@ -68,53 +68,6 @@ def get_dataset(dataset_name, root, source, target, train_source_transform, val_
     return train_source_dataset, train_target_dataset, val_dataset, test_dataset, num_classes, class_names
 
 
-def validate(val_loader, model, args, device) -> float:
-    batch_time = AverageMeter('Time', ':6.3f')
-    losses = AverageMeter('Loss', ':.4e')
-    top1 = AverageMeter('Acc@1', ':6.2f')
-    progress = ProgressMeter(
-        len(val_loader),
-        [batch_time, losses, top1],
-        prefix='Test: ')
-
-    # switch to evaluate mode
-    model.eval()
-    if args.per_class_eval:
-        confmat = ConfusionMatrix(len(args.class_names))
-    else:
-        confmat = None
-
-    with torch.no_grad():
-        end = time.time()
-        for i, (images, target) in enumerate(val_loader):
-            images = images.to(device)
-            target = target.to(device)
-
-            # compute output
-            output = model(images)
-            loss = F.cross_entropy(output, target)
-
-            # measure accuracy and record loss
-            acc1, = accuracy(output, target, topk=(1,))
-            if confmat:
-                confmat.update(target, output.argmax(1))
-            losses.update(loss.item(), images.size(0))
-            top1.update(acc1.item(), images.size(0))
-
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
-
-            if i % args.print_freq == 0:
-                progress.display(i)
-
-        print(' * Acc@1 {top1.avg:.3f}'.format(top1=top1))
-        if confmat:
-            print(confmat.format(args.class_names))
-
-    return top1.avg
-
-
 def get_train_transform(resizing='default', random_horizontal_flip=True, random_color_jitter=False):
     """
     resizing mode:
