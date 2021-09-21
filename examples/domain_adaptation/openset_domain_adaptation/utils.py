983b9474101bfb8c1caf571f9ce1b6f474245023
jiang <13126830206@163.com>
2021-09-22 02:31:13 +0800

    rename examples directory

---
+++ examples/domain_adaptation/openset_domain_adaptation/utils.py
@@ -0,0 +1,153 @@
+import sys
+import time
+import timm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as T
+
+sys.path.append('../../..')
+import common.vision.datasets.openset as datasets
+from common.vision.datasets.openset import default_open_set as open_set
+import common.vision.models as models
+from common.vision.transforms import ResizeImage
+from common.utils.metric import accuracy, ConfusionMatrix
+from common.utils.meter import AverageMeter, ProgressMeter
+
+
+def get_model_names():
+    return sorted(
+        name for name in models.__dict__
+        if name.islower() and not name.startswith("__")
+        and callable(models.__dict__[name])
+    ) + timm.list_models()
+
+
+def get_model(model_name):
+    if model_name in models.__dict__:
+        # load models from common.vision.models
+        backbone = models.__dict__[model_name](pretrained=True)
+    else:
+        # load models from pytorch-image-models
+        backbone = timm.create_model(model_name, pretrained=True)
+        try:
+            backbone.out_features = backbone.get_classifier().in_features
+            backbone.reset_classifier(0, '')
+            backbone.copy_head = backbone.get_classifier
+        except:
+            backbone.out_features = backbone.head.in_features
+            backbone.head = nn.Identity()
+            backbone.copy_head = lambda x: x.head
+    return backbone
+
+
+def get_dataset_names():
+    return sorted(
+        name for name in datasets.__dict__
+        if not name.startswith("__") and callable(datasets.__dict__[name])
+    )
+
+
+def get_dataset(dataset_name, root, source, target, train_source_transform, val_transform, train_target_transform=None):
+    if train_target_transform is None:
+        train_target_transform = train_source_transform
+    # load datasets from common.vision.datasets
+    dataset = datasets.__dict__[dataset_name]
+    source_dataset = open_set(dataset, source=True)
+    target_dataset = open_set(dataset, source=False)
+
+    train_source_dataset = source_dataset(root=root, task=source, download=True, transform=train_source_transform)
+    train_target_dataset = target_dataset(root=root, task=target, download=True, transform=train_target_transform)
+    val_dataset = target_dataset(root=root, task=target, download=True, transform=val_transform)
+    if dataset_name == 'DomainNet':
+        test_dataset = target_dataset(root=root, task=target, split='test', download=True, transform=val_transform)
+    else:
+        test_dataset = val_dataset
+    class_names = train_source_dataset.classes
+    num_classes = len(class_names)
+    return train_source_dataset, train_target_dataset, val_dataset, test_dataset, num_classes, class_names
+
+
+def get_train_transform(resizing='default', random_horizontal_flip=True, random_color_jitter=False):
+    """
+    resizing mode:
+        - default: resize the image to 256 and take a random resized crop of size 224;
+        - cen.crop: resize the image to 256 and take the center crop of size 224;
+        - res: resize the image to 224;
+        - res.|crop: resize the image to 256 and take a random crop of size 224;
+        - res.sma|crop: resize the image keeping its aspect ratio such that the
+            smaller side is 256, then take a random crop of size 224;
+        – inc.crop: “inception crop” from (Szegedy et al., 2015);
+        – cif.crop: resize the image to 224, zero-pad it by 28 on each side, then take a random crop of size 224.
+    """
+    if resizing == 'default':
+        transform = T.Compose([
+            ResizeImage(256),
+            T.RandomResizedCrop(224)
+        ])
+    elif resizing == 'cen.crop':
+        transform = T.Compose([
+            ResizeImage(256),
+            T.CenterCrop(224)
+        ])
+    elif resizing == 'res.':
+        transform = T.Resize(224)
+    elif resizing == 'res.|crop':
+        transform = T.Compose([
+            T.Resize((256, 256)),
+            T.RandomCrop(224)
+        ])
+    elif resizing == "res.sma|crop":
+        transform = T.Compose([
+            T.Resize(256),
+            T.RandomCrop(224)
+        ])
+    elif resizing == 'inc.crop':
+        transform = T.RandomResizedCrop(224)
+    elif resizing == 'cif.crop':
+        transform = T.Compose([
+            T.Resize((224, 224)),
+            T.Pad(28),
+            T.RandomCrop(224),
+        ])
+    else:
+        raise NotImplementedError(resizing)
+    transforms = [transform]
+    if random_horizontal_flip:
+        transforms.append(T.RandomHorizontalFlip())
+    if random_color_jitter:
+        transforms.append(T.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5))
+    transforms.extend([
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    return T.Compose(transforms)
+
+
+def get_val_transform(resizing='default'):
+    """
+    resizing mode:
+        - default: resize the image to 256 and take the center crop of size 224;
+        – res.: resize the image to 224
+        – res.|crop: resize the image such that the smaller side is of size 256 and
+            then take a central crop of size 224.
+    """
+    if resizing == 'default':
+        transform = T.Compose([
+            ResizeImage(256),
+            T.CenterCrop(224),
+        ])
+    elif resizing == 'res.':
+        transform = T.Resize((224, 224))
+    elif resizing == 'res.|crop':
+        transform = T.Compose([
+            T.Resize(256),
+            T.CenterCrop(224),
+        ])
+    else:
+        raise NotImplementedError(resizing)
+    return T.Compose([
+        transform,
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
