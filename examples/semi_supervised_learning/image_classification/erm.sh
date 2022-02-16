c6b96139321d64304dce329534942b0a97e80ccb
tsingcbx99 <57670068+tsingcbx99@users.noreply.github.com>
2022-02-16 09:23:48 +0800

    Update code for using unsupervised pre-trained models

--- examples/semi_supervised_learning/image_classification/erm.sh
+++ examples/semi_supervised_learning/image_classification/erm.sh
@@ -105,3 +105,142 @@ CUDA_VISIBLE_DEVICES=0 python erm.py data/caltech101 -d Caltech101 --num-samples
   -a resnet50 --seed 0 --log logs/erm/caltech_10_labels_per_class
 CUDA_VISIBLE_DEVICES=0 python erm.py data/caltech101 -d Caltech101 --oracle --finetune \
   -a resnet50 --epochs 80 --seed 0 --log logs/erm/caltech_oracle
+
+# ImageNet Unsupervised Pretrain (MoCov2, ResNet50)
+# ======================================================================================================================
+# Food 101
+CUDA_VISIBLE_DEVICES=0 python erm.py data/food101 -d Food101 --num-samples-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/food101_4_labels_per_class --lr 0.01 --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/food101 -d Food101 --num-samples-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/food101_10_labels_per_class --lr 0.01 --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/food101 -d Food101 --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/food101_oracle --lr 0.01 --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# CIFAR 10
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cifar10 -d CIFAR10 --train-resizing 'cifar' --val-resizing 'cifar' \
+  --norm-mean 0.4912 0.4824 0.4467 --norm-std 0.2471 0.2435 0.2616 --num-samples-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/cifar10_4_labels_per_class --lr 0.001 --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cifar10 -d CIFAR10 --train-resizing 'cifar' --val-resizing 'cifar' \
+  --norm-mean 0.4912 0.4824 0.4467 --norm-std 0.2471 0.2435 0.2616 --num-samples-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/cifar10_10_labels_per_class --lr 0.001 --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cifar10 -d CIFAR10 --train-resizing 'cifar' --val-resizing 'cifar' \
+  --norm-mean 0.4912 0.4824 0.4467 --norm-std 0.2471 0.2435 0.2616 --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/cifar10_oracle --lr 0.001 --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# CIFAR 100
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cifar100 -d CIFAR100 --train-resizing 'cifar' --val-resizing 'cifar' \
+  --norm-mean 0.5071 0.4867 0.4408 --norm-std 0.2675 0.2565 0.2761 --num-samples-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/cifar100_4_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cifar100 -d CIFAR100 --train-resizing 'cifar' --val-resizing 'cifar' \
+  --norm-mean 0.5071 0.4867 0.4408 --norm-std 0.2675 0.2565 0.2761 --num-samples-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/cifar100_10_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cifar100 -d CIFAR100 --train-resizing 'cifar' --val-resizing 'cifar' \
+  --norm-mean 0.5071 0.4867 0.4408 --norm-std 0.2675 0.2565 0.2761 --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/cifar100_oracle --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# CUB 200
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cub200 -d CUB200 --num-sample-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/cub200_4_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cub200 -d CUB200 --num-sample-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/cub200_10_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cub200 -d CUB200 --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/cub200_oracle --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# Aircraft
+CUDA_VISIBLE_DEVICES=0 python erm.py data/aircraft -d Aircraft --num-samples-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/aircraft_4_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/aircraft -d Aircraft --num-samples-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/aircraft_10_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/aircraft -d Aircraft --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/aircraft_oracle --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# StanfordCars
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cars -d StanfordCars --num-sample-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/car_4_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cars -d StanfordCars --num-sample-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/car_10_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/cars -d StanfordCars --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/car_oracle --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# SUN397
+CUDA_VISIBLE_DEVICES=0 python erm.py data/sun397 -d SUN397 --num-sample-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/sun_4_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/sun397 -d SUN397 --num-sample-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/sun_10_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/sun397 -d SUN397 --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/sun_oracle --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# DTD
+CUDA_VISIBLE_DEVICES=0 python erm.py data/dtd -d DTD --num-samples-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/dtd_4_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/dtd -d DTD --num-samples-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/dtd_10_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/dtd -d DTD --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/dtd_oracle --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# Oxford Pets
+CUDA_VISIBLE_DEVICES=0 python erm.py data/pets -d OxfordIIITPets --num-samples-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/pets_4_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/pets -d OxfordIIITPets --num-samples-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/pets_10_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/pets -d OxfordIIITPets --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/pets_oracle --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# Oxford Flowers
+CUDA_VISIBLE_DEVICES=0 python erm.py data/flowers -d OxfordFlowers102 --num-samples-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/flowers_4_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/flowers -d OxfordFlowers102 --num-samples-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/flowers_10_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/flowers -d OxfordFlowers102 --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/flowers_oracle --lr-scheduler cos --epochs 40 -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+
+# ======================================================================================================================
+# Caltech 101
+CUDA_VISIBLE_DEVICES=0 python erm.py data/caltech101 -d Caltech101 --num-samples-per-class 4 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/caltech_4_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/caltech101 -d Caltech101 --num-samples-per-class 10 --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/caltech_10_labels_per_class --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
+CUDA_VISIBLE_DEVICES=0 python erm.py data/caltech101 -d Caltech101 --oracle --finetune \
+  -a resnet50 --seed 0 --log logs/erm_moco_pretrain/caltech_oracle --lr-scheduler cos -i 2000 \
+  --pretrained-backbone checkpoints/moco_v2_800ep_backbone.pth
