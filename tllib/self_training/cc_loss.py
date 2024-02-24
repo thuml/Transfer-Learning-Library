86f24faa940f80bd4728cdb0f5cdb82c0341702c
Ying Jin <290895805@qq.com>
2024-02-24 15:43:43 +0800

    Add files via upload

--- tllib/self_training/cc_loss.py
+++ tllib/self_training/cc_loss.py
@@ -16,46 +16,31 @@ __all__ = ['CCConsistency']
 
 class CCConsistency(nn.Module):
     r"""
-    Minimum Class Confusion loss minimizes the class confusion in the target predictions.
-
-    You can see more details in `Minimum Class Confusion for Versatile Domain Adaptation (ECCV 2020) <https://arxiv.org/abs/1912.03699>`_
+    CC Loss attach class confusion consistency to MCC.
 
     Args:
         temperature (float) : The temperature for rescaling, the prediction will shrink to vanilla softmax if
           temperature is 1.0.
+        thr (float): The confidence threshold.
 
     .. note::
-        Make sure that temperature is larger than 0.
+        Make sure that temperature is larger than 0. Confidence threshold is larger than 0, smaller than 1.0.
 
     Inputs: g_t
         - g_t (tensor): unnormalized classifier predictions on target domain, :math:`g^t`
+        - g_t_strong (tensor): unnormalized classifier predictions on target domain, with strong data augmentation, :math:`g^t_{strong}`
 
     Shape:
-        - g_t: :math:`(minibatch, C)` where C means the number of classes.
+        - g_t, g_t_strong: :math:`(minibatch, C)` where C means the number of classes.
         - Output: scalar.
 
     Examples::
         >>> temperature = 2.0
-        >>> loss = MinimumClassConfusionLoss(temperature)
+        >>> loss = CCConsistency(temperature)
         >>> # logits output from target domain
         >>> g_t = torch.randn(batch_size, num_classes)
-        >>> output = loss(g_t)
-
-    MCC can also serve as a regularizer for existing methods.
-    Examples::
-        >>> from dalib.modules.domain_discriminator import DomainDiscriminator
-        >>> num_classes = 2
-        >>> feature_dim = 1024
-        >>> batch_size = 10
-        >>> temperature = 2.0
-        >>> discriminator = DomainDiscriminator(in_feature=feature_dim, hidden_size=1024)
-        >>> cdan_loss = ConditionalDomainAdversarialLoss(discriminator, reduction='mean')
-        >>> mcc_loss = MinimumClassConfusionLoss(temperature)
-        >>> # features from source domain and target domain
-        >>> f_s, f_t = torch.randn(batch_size, feature_dim), torch.randn(batch_size, feature_dim)
-        >>> # logits output from source domain adn target domain
-        >>> g_s, g_t = torch.randn(batch_size, num_classes), torch.randn(batch_size, num_classes)
-        >>> total_loss = cdan_loss(g_s, f_s, g_t, f_t) + mcc_loss(g_t)
+        >>> g_t_strong = torch.randn(batch_size, num_classes)
+        >>> output = loss(g_t, g_t_strong)
     """
 
     def __init__(self, temperature: float, thr=0.7):
